d71685e8a3ab0a451b77643ef7fd1ea9366df5a0
Ameetesh Sharma <ameetesh01@gmail.com>
2024-03-24 12:11:41 +0530

    Initial commit

---
+++ src/main/java/com/iplbetting/iplbettingtracker/IplBettingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.iplbetting.iplbettingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplBettingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplBettingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iplbetting/iplbettingtracker/IplBettingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iplbetting.iplbettingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplBettingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
